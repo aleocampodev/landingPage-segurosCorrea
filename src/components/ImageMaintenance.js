f8a233cad387a530bb835d41281fbacb9f3bdf2c
Aleja Ocampo <omaira.527@hotmail.com>
2021-12-17 12:13:10 -0500

    se agrega imagen mantenimiento

--- src/components/ImageMaintenance.js
+++ src/components/ImageMaintenance.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../assets/sass/main.scss';
+import maintance from '../assets/images/maintance.png';
 
 const imageMaintenance = () => (
   <div className="center-image">
-    <img
-      src="https://lh3.googleusercontent.com/proxy/c_wangTE4H44Gqpa-OKmJZxIUBGj0UK7iKi0wehTua195bb3oi61AYi_gyNI8l2X22shtjl5GhqIdbwtd-rL9lA"
-      alt="sitio en mantenimiento"
-    />
+    <img src={maintance} alt="sitio en mantenimiento" />
   </div>
 );
 
---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IndexPage = () => (
+  <section id="wrapper">
+    <header>
+      <div className="inner">
+        <h2>Page not found</h2>
+        <p>Not a valid URL</p>
+      </div>
+    </header>
+  </section>
+);
+
+export default IndexPage;
