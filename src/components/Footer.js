ae31781a06bd8afca7d64d15b365262bc1d02481
Aleja Ocampo <omaira.527@hotmail.com>
2022-02-02 22:23:31 -0500

    se modifica footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -70,7 +70,7 @@ export default function Footer() {
           name="contact"
           method="POST"
           data-netlify="true"
-          action="/pages/Confirmation"
+          action="/pages/confirmation"
           netlify-honeypot="bot-field"
           onSubmit={handleSubmit(onSubmit)}
         >
