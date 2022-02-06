fe564ee46eeaace191eddb3c19cd6736371d0393
Aleja Ocampo <omaira.527@hotmail.com>
2022-02-06 18:48:43 -0500

    modifica animacion

--- src/components/Services.js
+++ src/components/Services.js
@@ -36,16 +36,16 @@ const Services = () => {
       duration: 2000,
       easing: 'ease',
 
-      disable: true, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
       startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
       initClassName: 'aos-init', // class applied after initialization
       animatedClassName: 'aos-animate', // class applied on animation
-      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
       disableMutationObserver: false, // disables automatic mutations' detections (advanced)
       debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
       throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-      once: false, // whether animation should happen only once - while scrolling down
+      once: true, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
       anchorPlacement: 'top-bottom', // defines which position of the element
     });
