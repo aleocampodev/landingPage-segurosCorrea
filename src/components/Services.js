1066c48666434ba14b1db1028eec7cf30d9334fc
Aleja Ocampo <omaira.527@hotmail.com>
2022-02-06 19:36:23 -0500

    modificanso services

--- src/components/Services.js
+++ src/components/Services.js
@@ -34,23 +34,29 @@ const Services = () => {
     Aos.init({
       duration: 2000,
       easing: 'ease',
-      once:true,
+      once: true,
       animatedClassName: 'aos-animate', // class applied on animation
     });
   }, []);
+
+  useEffect(() => {
+    if (Aos) {
+      Aos.refresh();
+    }
+  });
 
   return (
     <section id="four" className="wrapper alt style1">
       <div className="inner" id="servicios">
         <HeadServices />
         <section className="features">
-          <div className="features-product" data-aos="fade-up">
+          <div className="features-product">
             {data.allMarkdownRemark.nodes
               .sort((a, b) => a.identifier - b.identifier)
               .map((node, index) => {
                 const text = node.html;
                 return (
-                  <div className="maincontainer" key={index}>
+                  <div className="maincontainer" key={index} data-aos="fade-up">
                     <div className="back">
                       <h2>{node.frontmatter.title}</h2>
                       <div dangerouslySetInnerHTML={{ __html: text }}></div>
