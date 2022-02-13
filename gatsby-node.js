4fd396095a845ad4246c550b0d6e7404a2b2e7c6
Aleja Ocampo <omaira.527@hotmail.com>
2022-02-13 00:41:45 -0500

    Moficando gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,17 +5,4 @@
  */
 
 // You can delete this file if you're not using it
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-  fmImagesToRelative(node);
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    });
-  }
-};
